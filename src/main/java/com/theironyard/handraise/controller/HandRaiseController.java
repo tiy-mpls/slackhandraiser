26236edb875c74cf9dbce128bdfb7f1512feb738
Kyle Rudy <kdrudy@Kyles-MacBook-Pro.local>
2016-10-07 13:33:29 -0500

    Updated to use @RestController

--- src/main/java/com/theironyard/handraise/controller/HandRaiseController.java
+++ src/main/java/com/theironyard/handraise/controller/HandRaiseController.java
@@ -12,11 +12,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by kdrudy on 10/7/16.
  */
-@Controller
+@RestController
 public class HandRaiseController {
 
 
