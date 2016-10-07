2f59c5fe4cd497ea9c8c0e539c30a645f84ce849
Kyle Rudy <kdrudy@Kyles-MacBook-Pro.local>
2016-10-07 15:25:06 -0500

    Added Bot configuration

---
+++ src/main/java/com/theironyard/handraise/component/HandRaisedBot.java
@@ -0,0 +1,33 @@
+package com.theironyard.handraise.component;
+
+import me.ramswaroop.jbot.core.slack.Bot;
+import me.ramswaroop.jbot.core.slack.Controller;
+import me.ramswaroop.jbot.core.slack.EventType;
+import me.ramswaroop.jbot.core.slack.models.Event;
+import me.ramswaroop.jbot.core.slack.models.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.socket.WebSocketSession;
+
+/**
+ * Created by kdrudy on 10/7/16.
+ */
+public class HandRaisedBot extends Bot {
+
+    @Value("${slackBotToken}")
+    private String slackToken;
+
+    @Override
+    public String getSlackToken() {
+        return slackToken;
+    }
+
+    @Override
+    public Bot getSlackBot() {
+        return this;
+    }
+
+    @Controller(events = {EventType.DIRECT_MESSAGE})
+    public void onReceiveDM(WebSocketSession session, Event event) {
+        reply(session, event, new Message("Hi, I am " + slackService.getCurrentUser().getName()));
+    }
+}
